8828ef0eefa31ee4cf701e82a0f148cfe4261071
VinoXuanyu <kimhyunwoo@buaa.edu.cn>
2022-05-17 18:33:00 +0800

    feat(recover): add panic handling

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"gee"
 	"html/template"
 	"log"
@@ -17,14 +18,22 @@ func onlyForV2() gee.HandlerFunc {
 }
 
 func main() {
-	engine := gee.New()
+	engine := gee.Default()
 	engine.LoadHTMLGlob("templates/*")
 	engine.SetFuncMap(template.FuncMap{})
-	engine.Use(gee.Logger())
 	engine.Static("/assets", "./static")
+
 	engine.GET("/", func(c *gee.Context) {
 		c.HTML(http.StatusOK, "css.tmpl", nil)
 	})
+
+	engine.GET("/panic", func(c *gee.Context) {
+		var a int
+		slice := make([]int, 0)
+		a = slice[100]
+		fmt.Println(a)
+	})
+
 	engine.GET("stress", func(c *gee.Context) {
 		temp := 0
 		for i := 0; i < math.MaxInt32; i++ {
@@ -63,7 +72,7 @@ func main() {
 			c.String(http.StatusOK, "This is a test for nested grouping")
 		})
 		v3.GET("/:name", func(c *gee.Context) {
-			c.String(http.StatusOK, c.Param("name)"))
+			c.String(http.StatusOK, "Hi, %s", c.Param("name"))
 		})
 	}
 	engine.Run(":9999")
